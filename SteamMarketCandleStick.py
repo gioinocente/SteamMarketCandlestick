5ac276a3c38796e23e7df329dde0ef87ae36b729
Giovane Winck <55024577+gioinocente@users.noreply.github.com>
2023-01-27 19:10:17 -0300

    2
    changes so the code can be executed by github actions properly

--- SteamMarketCandleStick.py
+++ SteamMarketCandleStick.py
@@ -3,42 +3,43 @@ import numpy as np
 import requests
 import json
 from datetime import datetime
-import time
 
 df = pd.read_csv("data.csv")
 url = "https://steamcommunity.com/market/priceoverview/?country=DE&currency=7&appid=730&market_hash_name=AK-47%20%7C%20Slate%20%28Field-Tested%29"
-high = 123456.1
-low = 0.1
 
-while True:
-    response = requests.get(url)
-    data = json.loads(response.text)
-    current_price = data["lowest_price"].replace("R$ ","").replace(",", ".")
-    
-    now = datetime.now()
-    current_day = now.strftime("%Y-%m-%d")
-    current_time = now.strftime("%H:%M")
-    
-    if current_time[-2:] == "00":
-        d2 = {'date': current_day, 'time': current_time, 'open': current_price, 'close': 0, 'high': current_price, 'low': current_price}
-        df2 = pd.DataFrame(d2, index=[0])
-        df = pd.concat([df, df2])
+response = requests.get(url)
+data = json.loads(response.text)
+current_price = data["lowest_price"].replace("R$ ","").replace(",", ".")
+
+now = datetime.now()
+current_day = now.strftime("%Y-%m-%d")
+current_time = now.strftime("%H")
+
+
+
+if df[(df['date'] == current_day) & (df['time'] == current_time)].empty:
+    # check if there is a previous row with a different time
+    previous_row = df[(df['date'] == current_day) & (df['time'] != current_time)].tail(1)
+    if not previous_row.empty:
+        previous_row.loc[:,'close'] = current_price
+        df.update(previous_row)
         df.to_csv("data.csv", index=False)
-        
-        current_hour = current_time
-        high = current_price
-        low = current_price
+    # add the new row to the frame
+    d2 = {'date': current_day, 'time': current_time, 'open': current_price, 'close': 0, 'high': current_price, 'low': current_price}
+    df2 = pd.DataFrame(d2, index=[0])
+    df = pd.concat([df, df2])
     
-    if current_time[-2:] == "59":
-        df.loc[np.logical_and(df['date'] == current_day, df['time'] == current_hour), 'close'] = current_price
+else:
+    # check if the current_price is lower then the low value for the current_hour
+    if float(current_price) < float(df.loc[(df['date'] == current_day) & (df['time'] == current_time)]['low']):
+        df.loc[np.logical_and(df['date'] == current_day, df['time'] == current_time), 'low'] = current_price
+        low = current_price
+    # check if the current_price is higher then the high value for the current_hour
+    if float(current_price) > float(df.loc[(df['date'] == current_day) & (df['time'] == current_time)]['high']):
+        df.loc[np.logical_and(df['date'] == current_day, df['time'] == current_time), 'high'] = current_price
         df.to_csv("data.csv", index=False)
-    
-    if float(current_price) < float(low):
-        df.loc[np.logical_and(df['date'] == current_day, df['time'] == current_hour), 'low'] = current_price
-        low = current_price
-    
-    if float(current_price) > float(high):
-        df.loc[np.logical_and(df['date'] == current_day, df['time'] == current_hour), 'high'] = current_price
-        high = current_price
 
 
+#mudar pra ler o ultimo registro do db pra ver se é a mesma hora
+#senão é criar uma linha com a hora nova
+#se for fazer os checks
