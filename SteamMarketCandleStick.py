4dc69895df6d518d8745af16eb4569b910281eab
Giovane Winck <55024577+gioinocente@users.noreply.github.com>
2023-02-03 09:06:52 -0300

    Update SteamMarketCandleStick.py

--- SteamMarketCandleStick.py
+++ SteamMarketCandleStick.py
@@ -46,7 +46,7 @@ else:
         df.to_csv("data.csv", index=False)
 
 df['time'] = pd.to_datetime(df['time'])
-df = df.drop(df.index[-1])
 df.set_index('time', inplace=True)
+df = df.drop(df.index[-1])
 mpf.plot(df,type='candle',style='charles', savefig='plot.png')
 
