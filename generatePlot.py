efe9a2f2e1b12e90fc9ed118e663c20738c2cbdd
Giovane Winck <55024577+gioinocente@users.noreply.github.com>
2023-02-03 09:00:44 -0300

    remove plot WF

--- generatePlot.py
+++
@@ -1,9 +0,0 @@
-import plotly.graph_objects as go
-import pandas as pd
-import mplfinance as mpf
-
-df = pd.read_csv('data.csv')
-df['time'] = pd.to_datetime(df['time'])
-df = df.drop(df.index[-1])
-df.set_index('time', inplace=True)
-mpf.plot(df,type='candle',style='charles', savefig='plot.png')
